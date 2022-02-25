3fd05aae6fa67edddb081fae6b9a1ec40e81a8be
wuranxu <619434176@qq.com>
2022-02-25 23:22:46 +0800

    add readme and document

---
+++ .umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'Pity使用文档',
+  mode: 'site',
+  base: '/pityDoc',
+  publicPath: '/pityDoc/'
+  // more config: https://d.umijs.org/config
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pity使用文档
+
+  [使用文档](https://wuranxu.github.io/pityDoc)
